58f9bdaaeb08ebf2330c08788983cbaf34990255
Matt Bechtel <bechma02@gmail.com>
2019-11-17 03:28:29 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,17 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item_word = req.path.split("/items/").last
+      item = @@items.find {|item| item.name == item_word}
+      resp.write item.price
+    else
+      
+  end
+
+end
